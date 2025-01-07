d6a6229a613c2d9bde9b37aaab79d75045ce9e32
scarletlvuyyuru <slvuyyuru@hotmail.com>
2025-01-07 13:01:12 -0500

    axios

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+
+function displayRecipe(response) {
+    
+        new Typewriter("#recipe", { 
+            strings: "Searching for a good recipe...",
+            autoStart: true,
+            delay: 1,
+            cursor: "", 
+        });
+
+}
+
+
 function generateForm(event) {
     event.preventDefault();
 let apiKey = "61e11tf2503b89498d076obf6bbaf870";
@@ -5,14 +18,7 @@ let prompt = "What are sourdough recipes using this topic";
 let context = "Provide example the the following format: Ingredients, Instructions";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`
 
-
-
-    new Typewriter("#recipe", { 
-        strings: "Searching for a good recipe...",
-        autoStart: true,
-        delay: 1,
-        cursor: "", 
-    });
+axios.get(apiUrl).then(displayRecipe);
 }
 
 let textForFormElement= document.querySelector("#textForForm");
